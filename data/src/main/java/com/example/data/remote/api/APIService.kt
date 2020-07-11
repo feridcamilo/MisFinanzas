d62ce85dd293aa3108cfaa51b3066e4f176fcc62
Ferid Camilo <feridcamilo@gmail.com>
2020-07-11 18:59:26 -0500

    Implemented retrofit

--- data/src/main/java/com/example/data/remote/api/APIService.kt
+++ data/src/main/java/com/example/data/remote/api/APIService.kt
@@ -1,9 +1,6 @@
 package com.example.data.remote.api
 
-import com.example.data.remote.model.APIParameterBody
-import com.example.data.remote.model.BalanceDTO
-import com.example.data.remote.model.CategoryDTO
-import com.example.data.remote.model.MovementDTO
+import com.example.data.remote.model.*
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -15,9 +12,18 @@ interface APIService {
     @GET("/Servicios/Maestros.svc/getCategorias/{clientId}")
     fun getCategories(@Path("clientId") clientId: String): Call<CategoryDTO>
 
+    @GET("/Servicios/Maestros.svc/getDeudas/{clientId}")
+    fun getDebts(@Path("clientId") clientId: String): Call<DebtDTO>
+
+    @GET("/Servicios/Maestros.svc/getLugares/{clientId}")
+    fun getPlaces(@Path("clientId") clientId: String): Call<PlaceDTO>
+
+    @GET("/Servicios/Maestros.svc/getPersonas/{clientId}")
+    fun getPeople(@Path("clientId") clientId: String): Call<PersonaDTO>
+
     @POST("/Servicios/Movimientos.svc/getSaldos")
     fun getSaldo(@Body parametros: APIParameterBody): Call<BalanceDTO>
 
     @POST("/Servicios/Movimientos.svc/getMovimientos")
-    fun getMovimientos(@Body parametros: APIParameterBody): Call<MovementDTO>
+    fun getMovements(@Body parametros: APIParameterBody): Call<MovementDTO>
 }
